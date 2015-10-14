4a34a1235c8a44a978b72f0edf0c1a3d5bb72e71
Kha Nguyen <hydrangea8604@gmail.com>
2015-10-14 17:54:15 +0700

    Update detail pages UI

--- AzureCloudService1/WebRoleAds/App_Start/RouteConfig.cs
+++ AzureCloudService1/WebRoleAds/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace WebRoleAds
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "TbPosts", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
